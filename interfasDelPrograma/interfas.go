323f942041f5883a156d45326ac87c9c66d7b2fd
JuanJL217 <juandocumentos217@gmail.com>
2024-06-04 02:10:02 -0300

    Texto agregado

--- interfasDelPrograma/interfas.go
+++ interfasDelPrograma/interfas.go
@@ -1,9 +1,12 @@
 package interfazArchivos
 
-type ejecucionArchivos interface {
+type EjecucionArchivos interface {
+	// Agrega la informacion del archivo
 	AgregarArchivo(string)
 
+	// Lista todos los IPs en el rango determinado
 	VerVisitantes(string, string)
 
+	// Se lista los N recursos más visitador
 	VerMasVisitados(int)
 }
